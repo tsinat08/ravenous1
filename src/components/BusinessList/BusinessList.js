8c37468f3b373e3f8d942cecea1ae4527d265d27
tsinat08 <tsinat.weldetnesae@gmail.com>
2019-11-30 20:31:18 -0500

    End pf part 2

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -2,16 +2,15 @@ import React from 'react';
 import './BusinessList.css';
 import Business from '../Business/Business';
 
+
 class BusinessList extends React.Component{
     render() {
         return (
-            <div className="BusinessList">
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
+            <div className="BusinessList">{
+                this.props.businesses.map((business) => {
+                    return <Business business= {business}/>
+            })
+                }
             </div>
         )
     }
